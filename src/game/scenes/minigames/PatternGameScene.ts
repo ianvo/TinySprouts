b262201acb0b8369c27b47dd0b49f91075d7b85b
Ian Vo <ianvo@Ians-MacBook-Air-2.local>
2024-12-20 11:47:09 -0800

    update pattern game to be button input instead of keyboard. adjust maximum possible generated values to stay within 20

--- src/game/scenes/minigames/PatternGameScene.ts
+++ src/game/scenes/minigames/PatternGameScene.ts
@@ -25,7 +25,7 @@ export class PatternGameScene extends GameScene
         this.camera.centerOn(0,0)
         this.camera.setBackgroundColor(0x000000);
         this.bgm.set("gameplay", new Howl({
-            src: ['assets/bgm/Sweet Treats.ogg'],
+            src: ['assets/bgm/Song_Exploration_02_Loop.ogg'],
             autoplay: true,
             loop: true,
             volume: .3
@@ -43,54 +43,60 @@ export class PatternGameScene extends GameScene
             volume: .5
         }));
         this.sfx.set("incorrect", new Howl({
-            src: ['assets/sfx/incorrect.ogg'],
+            src: ['assets/sfx/GAME_MENU_SCORE_SFX000603.ogg'],
             autoplay: false,
             loop: false,
             volume: .5
         }));
 
 
-        this.background = this.add.image(0, 0, 'background');
-        this.background.setAlpha(0.5);
+        this.background = this.add.image(-90, 100, 'classroom');
 
-        this.problemText = this.add.text(0, 0, '', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+        this.problemText = this.add.text(0, -110, '', {
+            fontFamily: 'Arial Black', fontSize: 68, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5).setDepth(100);
 
 
-        this.problemText = this.add.text(0, 0, '', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
 
+        for(var i = 0; i < 20; i++) {
+            this.addButton(i+1, (i%10 - 4.5) * 90, 100+Math.floor(i/10) * 90);
+        }
 
-        this.input?.keyboard?.on('keyup', this.handleKeyboardInput, this);
+        //this.input?.keyboard?.on('keyup', this.handleKeyboardInput, this);
         this.generateProblem();
         EventBus.emit('current-scene-ready', this);
     }
 
-    handleKeyboardInput(e: KeyboardEvent) {
-        if(e.key === "Enter") {
-            if(this.proposedAnswer !== "") {
-                this.submitAnswer();
-            }
-        }
-        else if(e.key === "Backspace") {
-            this.proposedAnswer = this.proposedAnswer.slice(0, -1);
-        }
-        else {
-            let number = parseInt(e.key);
-            if(!isNaN(number)) {
-                this.proposedAnswer += e.key;
-            }
-        }
+    addButton(value: number, x: number, y: number) {
+        let button = this.add.sprite(x, y, "button");
+        button.setScale(1.5);
+        button.setOrigin(.5, .5);
+        let text = this.add.text(x, y, `${value}`, {
+            fontFamily: 'Arial Black', fontSize: 32, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        });
+        text.setOrigin(.5, .5);
+
+        button.setInteractive();
+        button.on("pointerup", () => {
+            this.submitAnswer(value);
+        });
+
+        button.on("pointerover", () => {
+            button.setScale(1.6);
+        });
+
+        button.on("pointerout", () => {
+            button.setScale(1.5);
+        });
     }
 
-    submitAnswer() {
-        if(parseInt(this.proposedAnswer) === this.solution) {
+
+    submitAnswer(answer: number) {
+        if(answer === this.solution) {
             this.answering = false;
             this.problemText.text = "That's right!";
             this.sfx.get("correct")?.play();
@@ -99,34 +105,26 @@ export class PatternGameScene extends GameScene
             }, 1000);
         }
         else {
-            this.proposedAnswer = "";
             this.sfx.get("incorrect")?.play();
-            this.cameras.main.shake(200, 0.005);
+            this.cameras.main.shake(200, 0.002);
         }
     }
 
     generateProblem() {
         this.proposedAnswer = "";
         let diff = Math.floor(Math.random() * 4);
-        let currentVal = Math.floor(Math.random() * 10);
+        let currentVal = Math.floor(Math.random() * 8);
         this.problem = "";
         for (var i = 0; i < 4; i++) {
-            this.problem += `${currentVal}  `;
+            this.problem += `${currentVal}${i<3?"  ":""}`;
             currentVal += diff;
         }
         this.solution = currentVal;
         this.answering = true;
+        this.problemText.setText(this.problem);
     }
 
     update(t: number, dt:number) {
-        if(this.answering) {
-            if(this.proposedAnswer === "") {
-                this.problemText.text = `${this.problem}_?_`;
-            }
-            else {
-                this.problemText.text = `${this.problem}_${this.proposedAnswer}_`;
-            }
-        }
     }
 
     changeScene ()
